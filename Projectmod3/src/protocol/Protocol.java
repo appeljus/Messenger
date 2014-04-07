8ea2f289abbe33825b7a0a2a2174b6eb424285b7
kimberlyh <hengst.kimberly@gmail.com>
2014-04-07 12:15:08 +0200

    ik commit dit

---
+++ Projectmod3/src/protocol/Protocol.java
@@ -0,0 +1,12 @@
+package protocol;
+
+import java.net.*;
+import sun.net.*;
+
+public class Protocol {
+	int port = 4242;
+	String group = 192.168.5.
+	public static void main(String[] args) {
+		MulticastSocket socket = new MulticastSocket();
+	}
+}
