2e849904dc81583b4ad2470d64849a3c12eba750
appeljus <martijn.debijl@live.nl>
2014-04-07 14:48:51 +0200

    UDP protocol gemaakt

--- Projectmod3/src/protocol/Packet.java
+++ Projectmod3/src/protocol/Packet.java
@@ -1,10 +1,26 @@
 package protocol;
 
-public abstract class Packet {
+import org.apache.commons.lang3.ArrayUtils;
+import java.net.DatagramPacket;
 
-    private byte[] data;
+public class Packet {
 
-    public Packet(byte[] data){
+    public static byte[] getByteArray(byte[] sourcePort, byte[] destinationPort, byte[] checksum, byte[] sequencenumber, byte[] hopCount, byte[] data){
 
+        byte[] result;
+        result = ArrayUtils.addAll(sourcePort, destinationPort);
+        result = ArrayUtils.addAll(result, checksum);
+        result = ArrayUtils.addAll(result, sequencenumber);
+        result = ArrayUtils.addAll(result, hopCount);
+        result = ArrayUtils.addAll(result, data);
+
+        return result;
+    }
+
+    public static DatagramPacket makePacket(byte[] sourcePort, byte[] destinationPort, byte[] checksum, byte[] sequencenumber, byte[] hopCount, byte[] data){
+        byte[] byteArray = getByteArray(sourcePort, destinationPort, checksum, sequencenumber, hopCount, data);
+        int lengte = byteArray.length;
+        DatagramPacket packet = new DatagramPacket(byteArray, lengte);
+        return packet;
     }
 }
