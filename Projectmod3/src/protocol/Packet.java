4e81ed856697fe2766c5d522e2bd6d80ce0b1a1e
appeljus <martijn.debijl@live.nl>
2014-04-07 14:57:34 +0200

    Andere constructor datagrampacket

--- Projectmod3/src/protocol/Packet.java
+++ Projectmod3/src/protocol/Packet.java
@@ -2,14 +2,14 @@ package protocol;
 
 import org.apache.commons.lang3.ArrayUtils;
 import java.net.DatagramPacket;
+import java.net.InetAddress;
 
 public class Packet {
 
-    public static byte[] getByteArray(byte[] sourcePort, byte[] destinationPort, byte[] checksum, byte[] sequencenumber, byte[] hopCount, byte[] data){
+    public static byte[] getByteArray(byte[] sourcePort, byte[] checksum, byte[] sequencenumber, byte[] hopCount, byte[] data){
 
         byte[] result;
-        result = ArrayUtils.addAll(sourcePort, destinationPort);
-        result = ArrayUtils.addAll(result, checksum);
+        result = ArrayUtils.addAll(sourcePort, checksum);
         result = ArrayUtils.addAll(result, sequencenumber);
         result = ArrayUtils.addAll(result, hopCount);
         result = ArrayUtils.addAll(result, data);
@@ -17,10 +17,10 @@ public class Packet {
         return result;
     }
 
-    public static DatagramPacket makePacket(byte[] sourcePort, byte[] destinationPort, byte[] checksum, byte[] sequencenumber, byte[] hopCount, byte[] data){
-        byte[] byteArray = getByteArray(sourcePort, destinationPort, checksum, sequencenumber, hopCount, data);
+    public static DatagramPacket makePacket(InetAddress adres, int port, byte[] sourcePort, byte[] checksum, byte[] sequencenumber, byte[] hopCount, byte[] data){
+        byte[] byteArray = getByteArray(sourcePort, checksum, sequencenumber, hopCount, data);
         int lengte = byteArray.length;
-        DatagramPacket packet = new DatagramPacket(byteArray, lengte);
+        DatagramPacket packet = new DatagramPacket(byteArray, lengte, adres, port);
         return packet;
     }
 }
