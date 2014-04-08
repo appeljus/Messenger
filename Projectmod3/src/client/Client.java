a70f5e868ea54f4884cf20a1f4a93a199fb840c2
kimberlyh <hengst.kimberly@gmail.com>
2014-04-08 13:10:26 +0200

    Client naar mutlicast

--- Projectmod3/src/client/Client.java
+++ Projectmod3/src/client/Client.java
@@ -9,33 +9,41 @@ import protocol.*;
 
 public class Client {
 	ChatWindow chatwindow;
-	
+
 	static int port = 4242;
 	String id1 = "192.168.5.1";
 	String id2 = "192.168.5.2";
 	String id3 = "192.168.5.3";
 	String id4 = "192.168.5.4";
 	MulticastSocket s;
-	ArrayList<String> list = new ArrayList<String>();
-	
-	
+	InetAddress group;
+	//	ArrayList<String> list = new ArrayList<String>();
+
+
 	public static void main(String[] args) {
 	}
-	
+
 	public Client(ChatWindow c) {
 		chatwindow = c;
 		try {
+			group = InetAddress.getByName("228.5.6.7");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
 			s = new MulticastSocket(port);
+			s.joinGroup(group);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		list.add(id1);
-		list.add(id2);
-		list.add(id3);
-		list.add(id4);
+		//		list.add(id1);
+		//		list.add(id2);
+		//		list.add(id3);
+		//		list.add(id4);
 	}
-	
+
 	public String getIP() {
 		String result = null;
 		try {
@@ -46,17 +54,17 @@ public class Client {
 		}
 		return result;
 	}
-	
+
 	public void receivePacket() { 
 		try {
 			byte[] buf = new byte[1024];
 			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			
+
 			while(true) {
 				s.receive(packet);
 				byte[] receiveData = packet.getData();
 				String destination = packet.getAddress().toString();
-				if (destination != this.getIP()) {
+				if (!destination.equals(this.getIP())) {
 					s.send(packet);
 				}
 			}
@@ -65,26 +73,15 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void sendPacket(String message) {
 		byte[] data = message.getBytes();
-		for (int i = 0; i<list.size(); i++) {
-			InetAddress address = null;
-			if(list.get(i) != this.getIP()) {
-				try {
-					address = InetAddress.getByName(list.get(i));
-				} catch (UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				DatagramPacket packetToSend = new DatagramPacket(data, data.length, address, port);
-				try {
-					s.send(packetToSend);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}	
+		DatagramPacket packetToSend = new DatagramPacket(data, data.length, group, port);
+		try {
+			s.send(packetToSend);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
