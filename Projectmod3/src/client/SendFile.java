db4892976a9bb7e2fa9443cafe1d3aeaffa3c334
Tim108 <t.sonderen@hotmail.com>
2014-04-11 12:23:39 +0200

    bezig..

--- Projectmod3/src/client/SendFile.java
+++ Projectmod3/src/client/SendFile.java
@@ -2,10 +2,13 @@ package client;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.apache.commons.io.*;
 
 public class SendFile implements Runnable{
 	Client c;
@@ -14,6 +17,7 @@ public class SendFile implements Runnable{
 	int port;
 	byte[] fileParts;
 	String ext;
+	String name;
 	
 	public SendFile(Client c, File f, InetAddress target, int port){
 		this.c = c;
@@ -29,12 +33,27 @@ public class SendFile implements Runnable{
 			System.out.println("Fuck the File!");
 		}
 		
-		String ext = FilenameUtils.getExtension(path);
+		ext = FilenameUtils.getExtension(pathString);
+		name = FilenameUtils.getName(pathString);
+		
 	}
 
 	@Override
 	public void run() {
+		DatagramPacket packetToSend = new DatagramPacket(data, data.length,
+				group, port);
 		
+		lastMsgs.add(packetToSend);
+		if(lastMsgs.size() > BUFFER_SIZE){
+			lastMsgs.remove(0);
+		}
+		try {
+			s.send(packetToSend);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		incrementSeqNr();
 	}
 	
 }
