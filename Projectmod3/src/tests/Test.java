3a82295521672214544bfe7f89249cd496c5267d
Tim108 <t.sonderen@hotmail.com>
2014-04-15 11:55:10 +0200

    fist test made

--- Projectmod3/src/tests/Test.java
+++ Projectmod3/src/tests/Test.java
@@ -1,48 +1,62 @@
 package tests;
 
+import java.util.ArrayList;
+
 import GUI.ChatWindow;
 
-public class Test extends Thread{
+public class Test extends Thread {
 	ChatWindow c;
-	
-	public Test(){
+
+	String lastReceived = null;
+
+	ArrayList<String> received = new ArrayList<String>();
+
+	public Test() {
 		c = new ChatWindow("Tester", this);
 	}
-	
-	public void run(){
-		
+
+	public void run() {
+		basicConnection();
 	}
-	
+
 	// all tests here
-	private void basicConnection(){
+	private void basicConnection() {
 		String sentMsg = "Awesome test message";
 		c.addText(c.generateLine(sentMsg));
-		String receivedMsg = null;
-		while (receivedMsg != null){
-			
+		while (lastReceived == null) {
+			try {
+				sleep(200);
+			} catch (InterruptedException e) {
+				System.out.println("HIJ WIL NIET TUKKEN! :O");
+			}
 		}
+		printTestResult("Test for the basic message sending", sentMsg, lastReceived);
+		lastReceived = null;
 	}
-	
+
 	// utills here
-	
-	public void incoming(String txt){
-		
+
+	public void incoming(String txt) {
+		if (lastReceived != null)
+			lastReceived = txt;
+		received.add(txt);
 	}
-	
-	private void printTestResult(String description, Object expected, Object result){
+
+	private void printTestResult(String description, Object expected,
+			Object result) {
 		System.out.println("--- New test ---");
 		System.out.println(description);
 		System.out.println("Expected: " + expected.toString());
 		System.out.println("Result: " + result.toString());
 		System.out.print("Test result: ");
-		if(expected.equals(result))
+		if (expected.equals(result))
 			System.out.println("Positive");
 		else
 			System.out.println("Negative");
 		System.out.println("--- End of test ---");
 	}
-	
-	public static void main(String[] arg0){
+
+	public static void main(String[] arg0) {
 		Thread thread = new Thread(new Test());
 		thread.start();
 	}
