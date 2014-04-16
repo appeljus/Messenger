0be33741e343b24799ccd4a11dfb6963e276e4ae
Kevkev1 <kevin1226@live.nl>
2014-04-16 13:27:06 +0200

    Encryptie

--- Projectmod3/src/client/Encryption2.java
+++ Projectmod3/src/client/Encryption2.java
@@ -1,40 +1,43 @@
 package client;
 
-import java.nio.ByteBuffer;
-
 public class Encryption2 {
 	
-	int shift;
+	int[] shift = new int[4];
 	
 	public Encryption2(){
-		shift = shift + (int)"D".getBytes()[0];
-		shift = shift + (int)"o".getBytes()[0];
-		shift = shift + (int)"i".getBytes()[0];
-		shift = shift + (int)"f".getBytes()[0];
-		System.out.println(shift + "");
+		shift[0] = ((int)"D".getBytes()[0]) & 0xFF;
+		shift[1] = ((int)"o".getBytes()[0]) & 0xFF;
+		shift[2] = ((int)"i".getBytes()[0]) & 0xFF;
+		shift[3] = ((int)"f".getBytes()[0]) & 0xFF;
 	}
 	
 	public byte[] encrypt(byte[] data){
 		for (int i = 0; i < data.length; i++){
-			data[i] = (byte)(data[i] - shift);
+			if(i % 4 == 0) data[i] = (byte)(data[i] - shift[3]);
+			if(i % 3 == 0) data[i] = (byte)(data[i] - shift[2]);
+			if(i % 2 == 0) data[i] = (byte)(data[i] - shift[1]);
+			else data[i] = (byte)(data[i] - shift[0]);
 		}
 		return data;
 	}
 	
 	public byte[] decrypt(byte[] data){
 		for (int i = 0; i < data.length; i++){
-			data[i] = (byte)(data[i] + shift);
+			if(i % 4 == 0) data[i] = (byte)(data[i] + shift[3]);
+			if(i % 3 == 0) data[i] = (byte)(data[i] + shift[2]);
+			if(i % 2 == 0) data[i] = (byte)(data[i] + shift[1]);
+			else data[i] = (byte)(data[i] + shift[0]);
 		}
 		return data;
 	}
 	
 	public int getShift(){
-		return shift;
+		return shift[0];
 	}
 
 	public static void main(String[] args){
 		Encryption2 encryption = new Encryption2();
-		byte[] hoi = "Hallo".getBytes();
+		byte[] hoi = "Hallo dit is een Doif-encryptie.".getBytes();
 		hoi = encryption.encrypt(hoi);
 		System.out.println(new String(hoi));
 		hoi = encryption.decrypt(hoi);
